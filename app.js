258143f2fe347f2806e70e3fa16fa0955fc0e802
Damfler <gashuk2002@yandex.ru>
2021-09-23 22:53:01 +0300

    new func

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const slides = document.querySelectorAll('.slide')
+function slidePlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
 
 // way 1
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
 
-        slide.classList.add('active')
-    })
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        // way 2
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    // way 2
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidePlugin(4)
